d35d54665e347d955927e8464b53c6d06057ab33
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2023-06-27 17:31:21 +0200

    ajout de mon nom auteur

--- ok.py
+++ ok.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.write("hello world")
+st.write("AUTEUR : Mr.DJEGUI-WAGUÉ")
 st.title('VISUALISATION')
 st.subheader("merci pour votre analyse")
 
