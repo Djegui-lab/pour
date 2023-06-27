e48e9bd732c80e6210ed2606edb7ec77912ff97d
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2023-06-27 16:12:18 +0200

    ajout gitignore

---
+++ ok.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.write("hello world")
+st.title('VISUALISATION')
+st.subheader("merci pour votre analyse")
+
+uploaded_file = st.file_uploader('Choisir un fichier XLSX ', type='xlsx')
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    st.dataframe(df)
+
+    groupby_column = st.selectbox(
+        "Qu'aimeriez-vous analyser ?",
+        ('Nombre_de_sinistres', 'Age_du_conducteur', 'Puissance_fiscale', 'Annees_assurance','Date_de_permis','annee_de_mise _en_circulation'),
+    )
+    # -- GROUP DATAFRAME
+    output_columns = ['Annees_assurance', 'Age_du_conducteur']
+    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].mean()
+    st.dataframe(df_grouped)
+    # -- PLOT DATAFRAME
+    fig = px.bar(
+        df_grouped,
+        x=groupby_column,
+        y='Age_du_conducteur',
+        color='Annees_assurance',
+        color_continuous_scale=['red', 'yellow', 'green'],
+        template='plotly_white',
+        title=f'<b>age du conducteur & année assurance by {groupby_column}</b>'
+    )
+    st.plotly_chart(fig)
