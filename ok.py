90f921155351a9f4f31d3ae79d884ea2a130d86a
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2023-09-09 22:45:13 +0200

    Update ok.py

--- ok.py
+++ ok.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 import sqlite3
 st.set_page_config(page_title='djégui', layout='wide')
