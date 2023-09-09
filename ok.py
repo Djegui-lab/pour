fff2162e2c385c7ebdb3d86c9baca16e9407e853
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2023-09-09 22:40:12 +0200

    Update ok.py

--- ok.py
+++ ok.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import sqlite3
-import matplotlib.pyplot as plt
 st.set_page_config(page_title='djégui', layout='wide')
 
 # Saisie du nom de l'utilisateur
